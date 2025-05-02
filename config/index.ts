84d43ff28c9efa5120da6d422afa4b9642f67bb8
Adel2411 <hadjarabadel.2411@gmail.com>
2025-05-02 07:34:20 +0100

    Initial commit to setup the project

---
+++ config/index.ts
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Languini",
+  description:
+    "Interactive platform for language learning with lessons, quizzes, and progress tracking.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "languini",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "languini",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "HADJ ARAB Adel",
+    url: "https://github.com/Adel2411",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/Adel2411/Languini",
+  email: "hadjrabadel.2411@gmail.com",
+} as const;
