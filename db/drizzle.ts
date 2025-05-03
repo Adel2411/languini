8bc12c04ead3f03350fd208be7c00c3c11fc9fb7
Adel2411 <hadjarabadel.2411@gmail.com>
2025-05-03 02:30:53 +0100

    Changed the db to a neon hosted postgres db

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,23 +1,9 @@
-// import { neon } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-http";
-//
-// import * as schema from "./schema";
-//
-// const sql = neon(process.env.DATABASE_URL);
-// const db = drizzle(sql, { schema });
-//
-// export default db;
-
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-// Create a PostgreSQL connection pool
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const db = drizzle(pool, { schema });
+const sql = neon(process.env.DATABASE_URL);
+const db = drizzle(sql, { schema });
 
 export default db;
